58b60c1d00e05a523e9ca8ada4b9418b496663ec
Carlos Iribarren <iribarrenzubizarreta@gmail.com>
2015-02-19 00:12:20 +0100

    Proyecto Abestik añadido. Abestik project added.

---
+++ Abestik/Bezeroa.h
@@ -0,0 +1,13 @@
+#define MAX_BUF 4096
+#define SERVER "localhost"
+#define PORT 6012
+#define FILES_PATH	"/home/telmo/Escritorio/JETSITAKO_KANTAK"
+
+
+void kredituak();
+int hasierakomenua();
+void BILAKETA();
+void EXIT();
+void JEITSI();
+int erabiltzailea();
+int pass();
